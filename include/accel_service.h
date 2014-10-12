040f1f5233feaf3ea8fa9dd9d7b1df4ca111ccd3
Jan Machacek <jan.machacek@gmail.com>
2014-10-12 09:31:29 +0100

    Code drop

--- include/accel_service.h
+++ include/accel_service.h
@@ -8,18 +8,24 @@ extern "C" {
 namespace pebble {
 namespace mock {
 
+    /**
+     * AccelService controls the Pebble AS mocks. Its ``<<`` operators
+     * provide mechanism to push the accelerometer data.
+     *
+     * Any callbacks will be triggered after pushing the data in.
+     */
     class AccelService {
     private:
         std::vector<AccelRawData> raw_data;
         int samples_per_update;
         AccelRawDataHandler raw_data_handler;
+
+        void trigger_raw();
     public:
         AccelService& operator<<(const AccelRawData data);
         AccelService& operator<<(const std::vector<AccelRawData> &data);
 
         void accel_raw_data_service_subscribe(uint32_t samples_per_update, AccelRawDataHandler handler);
-
-        void trigger_raw();
     };
 
 }
